95abe8c6b6c360c41004521a6ce9abf338904c4f
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2023-05-10 11:07:40 +0530

    Session 1

--- session-1/App.js
+++ session-1/App.js
@@ -1,6 +1,20 @@
- // creating an h1 element using react createElement API
- const greeting = React.createElement("h1",{},"Hello developers , welcome all");
- // creating root using createRoot API where the data need to be rendered 
- const root = ReactDOM.createRoot(document.getElementById("root"));
- // finally using render method we are rendering the content
- root.render(greeting);
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement("h1", {}, "Hello guys"),React.createElement("h1", {}, "How are you doing?")]
+  )
+);
+
+// creating an h1 element using react createElement API
+const greeting = React.createElement(
+  "h1",
+  {},
+  "Hello developers , welcome all"
+);
+// creating root using createRoot API where the data need to be rendered
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// finally using render method we are rendering the content
+root.render(heading);
