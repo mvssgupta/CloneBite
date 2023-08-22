69513eb82b40c9ff60c6720c95ace7d8d98db9d2
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2023-08-22 21:35:43 +0530

    used realtime data from swiggy API using axios

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,35 @@
-import { useState } from "react";
-import { resDetails } from "../utils/constants";
+import { useState,useEffect } from "react";
 import RestaurantCardComponent from './RestaurantCardComponent'
+import axios from "axios";
+import ShimmerUI from "./ShimmerUI";
 
 const Body = () => {
-  const[listOfRestaurants,setListOfRestaurants] = useState(resDetails);
+  const[listOfRestaurants,setListOfRestaurants] = useState([]);
+
+  useEffect(()=>{
+    fetchDataFromApi();
+  },[])
+
+  const fetchDataFromApi =  async() => {
+    const res = await axios.get("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.6868159&lng=83.2184815&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    const resData = res.data
+    setListOfRestaurants(resData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  }
+
+  if(listOfRestaurants?.length===0)
+  {
+    return <ShimmerUI></ShimmerUI>
+  }
 
     return (
       <div className="body">
        <div className="search-bar">
             <input type="text" placeholder="Search" />
             <button>Search</button>
-            <button onClick={()=> setListOfRestaurants(resDetails.filter(data => data.info.avgRating>4))}>Top Rated Restaurants</button>
+            <button onClick={()=> setListOfRestaurants(listOfRestaurants.filter(data => data.info.avgRating>=4))}>Top Rated Restaurants</button>
           </div>
         <div className="restaurant-container">
-        {listOfRestaurants.map((restaurant) => (<RestaurantCardComponent key={restaurant.info.id} resData = {restaurant}/>))} 
+        {listOfRestaurants?.map((restaurant) => (<RestaurantCardComponent key={restaurant.info.id} resData = {restaurant}/>))} 
         </div>
       </div>
     );
---
+++ src/components/ShimmerUI.js
@@ -0,0 +1,39 @@
+const ShimmerUI = () => {
+    return (
+            <div className="shimmerUI">
+                <div className="shimmer-card">
+                    <div className="shimmer-img"></div>
+                </div>
+                <div className="shimmer-card">
+                    <div className="shimmer-img"></div>
+                </div>
+                <div className="shimmer-card">
+                    <div className="shimmer-img"></div>
+                </div>
+                <div className="shimmer-card">
+                    <div className="shimmer-img"></div>
+                </div>
+                <div className="shimmer-card">
+                    <div className="shimmer-img"></div>
+                </div>
+                <div className="shimmer-card">
+                    <div className="shimmer-img"></div>
+                </div>
+                <div className="shimmer-card">
+                    <div className="shimmer-img"></div>
+                </div>
+                <div className="shimmer-card">
+                    <div className="shimmer-img"></div>
+                </div>
+                <div className="shimmer-card">
+                    <div className="shimmer-img"></div>
+                </div>
+                <div className="shimmer-card">
+                    <div className="shimmer-img"></div>
+                </div>
+               
+            </div>
+        )
+}
+
+export default ShimmerUI;
